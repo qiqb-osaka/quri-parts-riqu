318ff2f4a907de746a62315330c2e3b8661c48f6
HamadaKazuki <hamada.kazuki@sec.co.jp>
2024-03-02 03:44:24 +0900

    Add client functions for SSE

--- quri_parts/riqu/rest/api/job_api.py
+++ quri_parts/riqu/rest/api/job_api.py
@@ -123,6 +123,101 @@ class JobApi(object):
             _request_timeout=params.get('_request_timeout'),
             collection_formats=collection_formats)
 
+    def download_file(self, job_id, **kwargs):  # noqa: E501
+        """Download file  # noqa: E501
+
+        download file of the job.  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.download_file(job_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param str job_id: Job ID (required)
+        :return: object
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.download_file_with_http_info(job_id, **kwargs)  # noqa: E501
+        else:
+            (data) = self.download_file_with_http_info(job_id, **kwargs)  # noqa: E501
+            return data
+
+    def download_file_with_http_info(self, job_id, **kwargs):  # noqa: E501
+        """Download file  # noqa: E501
+
+        download file of the job.  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.download_file_with_http_info(job_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param str job_id: Job ID (required)
+        :return: object
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['job_id']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method download_file" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'job_id' is set
+        if ('job_id' not in params or
+                params['job_id'] is None):
+            raise ValueError("Missing the required parameter `job_id` when calling `download_file`")  # noqa: E501
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'job_id' in params:
+            path_params['job_id'] = params['job_id']  # noqa: E501
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = []  # noqa: E501
+
+        return self.api_client.call_api(
+            '/jobs/{job_id}/download', 'GET',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='object',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
     def get_job(self, job_id, **kwargs):  # noqa: E501
         """Get Job  # noqa: E501
 
@@ -306,6 +401,103 @@ class JobApi(object):
             post_params=form_params,
             files=local_var_files,
             response_type='InlineResponse201',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
+    def post_ssejob(self, **kwargs):  # noqa: E501
+        """Post SSE Job  # noqa: E501
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.post_ssejob(async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param str up_file:
+        :param str job_type:
+        :return: object
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.post_ssejob_with_http_info(**kwargs)  # noqa: E501
+        else:
+            (data) = self.post_ssejob_with_http_info(**kwargs)  # noqa: E501
+            return data
+
+    def post_ssejob_with_http_info(self, **kwargs):  # noqa: E501
+        """Post SSE Job  # noqa: E501
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.post_ssejob_with_http_info(async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param str up_file:
+        :param str job_type:
+        :return: object
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['up_file', 'job_type']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method post_ssejob" % key
+                )
+            params[key] = val
+        del params['kwargs']
+
+        collection_formats = {}
+
+        path_params = {}
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+        if 'up_file' in params:
+            local_var_files['up_file'] = params['up_file']  # noqa: E501
+        if 'job_type' in params:
+            form_params.append(('job_type', params['job_type']))  # noqa: E501
+
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # HTTP header `Content-Type`
+        header_params['Content-Type'] = self.api_client.select_header_content_type(  # noqa: E501
+            ['multipart/form-data'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = []  # noqa: E501
+
+        return self.api_client.call_api(
+            '/ssejobs', 'POST',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='object',  # noqa: E501
             auth_settings=auth_settings,
             async_req=params.get('async_req'),
             _return_http_data_only=params.get('_return_http_data_only'),
